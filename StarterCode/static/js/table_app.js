f9f48477d81c8656934f3d211c0e8da3cdbbe777
Carrie Chunn <crchunn@gmail.com>
2020-08-15 16:01:37 -0500

    finalized some code

--- StarterCode/static/js/table_app.js
+++ StarterCode/static/js/table_app.js
@@ -6,27 +6,35 @@ table.attr("class", "table table-striped");
 // console.log(data)
 
 
+var incident = data
+
+function dumpTable(data) {
+    tbody.html("");
 
 
-// Select the button
-var button = d3.select("#button");
 
-// Select the form
-var form = d3.select("#form");
+    // Select the button
+    var button = d3.select("#filter-btn");
 
-// Create event handlers 
-button.on("click", runEnter);
-form.on("submit", runEnter);
+    // Select the form
+    var form = d3.select("#form");
 
-data.forEach((incident) => {
-    var row = tbody.append("tr");
-    Object.entries(incident).forEach(([key, value]) => {
-        var cell = row.append("td");
-        cell.text(value);
+    // Create event handlers 
+    d3.selectAll(".filter").on("change", runEnter);
+    // button.on("click", runEnter);
+    // form.on("submit", runEnter);
+
+    data.forEach((incident) => {
+        var row = tbody.append("tr");
+        Object.entries(incident).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
     });
-});
-
+}
+dumpTable(incident);
 // console.log(incident)
+
 
 // Complete the event handler function for the form
 function runEnter() {
@@ -35,17 +43,29 @@ function runEnter() {
     d3.event.preventDefault();
 
     // Select the input element and get the raw HTML node
-    var inputElement = d3.select("date");
+    var inputElement = d3.select(this).select("input");
 
+    // var inputState = d3.select("#state");
+    console.log(inputElement)
     // Get the value property of the input element
     var inputValue = inputElement.property("value");
+    // var inputStateValue = inputState.property('value');
 
     console.log(inputValue);
-    // console.log(incident);
+    var filters = {}
+    var filterid = inputElement.attr("id")
+    Object.entries(filters)
+    if (inputElement) {
+        filters[filterid] = inputValue
+    };
+    var filteredData = incident
 
-    var filteredData = incidents.filter(incident => incident.datetime === inputValue);
+    // var filteredData = filteredData.filter(incident => incident.datetime === inputValue);
+    Object.entries(filters).forEach(([key, value]) => {
+        filteredData = filteredData.filter(row => row[key] == value)
+    });
 
     console.log(filteredData);
-
+    dumpTable(filteredData);
 
 }
